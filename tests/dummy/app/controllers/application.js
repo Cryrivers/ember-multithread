48f49b4b24943225ee6c081a30de71167fad2a30
Wang Zhongliang <wangz@garena.com>
2016-08-26 16:00:39 +0800

    Update DEMO

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import {worker} from 'ember-multithread';
 
-const ITERATION_COUNT = 50000000;
+const ITERATION_COUNT = 500000000;
 
 function _piCalculation(iteration, updateProgress = false) {
   let pi = 0;
@@ -10,25 +10,32 @@ function _piCalculation(iteration, updateProgress = false) {
     pi = pi + (4 / n) - (4 / (n + 2));
     n = n + 4;
     if (updateProgress) {
-      this.set('progress', (i / iteration * 100).toFixed());
+      this.setProperties({
+        pi,
+        progress: (i / iteration * 100).toFixed()
+      });
     }
   }
   return pi;
 }
 
 export default Ember.Controller.extend({
+  finialPi: 0,
+  pi: 0,
   progress: 0,
   calculatePiWorker: worker(_piCalculation),
   actions: {
     calculateSingleThreaded() {
       this.set('progress', 0);
       const result = _piCalculation.call(this, ITERATION_COUNT);
-      console.log(result);
+      this.set('pi', 0);
+      this.set('finalPi', result);
     },
     calculate() {
       this.set('progress', 0);
-      this.get('calculatePiWorker').perform(ITERATION_COUNT).then(result=> {
-        console.log(result);
+      this.get('calculatePiWorker').perform(ITERATION_COUNT, true).then(result=> {
+        this.set('pi', 0);
+        this.set('finalPi', result);
       });
     }
   }
